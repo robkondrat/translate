17902abed3fcd6543bc3162c74568b5231c76c70
EatYourSoup7 <r.kondratowicz1@gmail.com>
2021-02-02 17:55:57 -0600

    setup basic components, ready to wire up context

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import UserCreate from './UserCreate';
 
 class App extends React.Component {
+  state = { language: 'english' };
+  
+  onLanguageChange = language => {
+    this.setState({ language });
+  }
+  
   render() {
     return (
-      <div>
-        App
+      <div className="ui container">
+        <div>
+          Select a language: 
+          <i className="flag us" onClick={() => this.onLanguageChange('english')} />
+          <i className="flag pl" onClick={() => this.onLanguageChange('polish')} />
+        </div>
+        <UserCreate />
       </div>
     )
   }
-}
+};
 
 export default App;
