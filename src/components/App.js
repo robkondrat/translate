605cc36b1165f04b6a212c5dde202a8bd0e7f088
EatYourSoup7 <r.kondratowicz1@gmail.com>
2021-02-02 18:46:31 -0600

    access data from multiple contexts

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import UserCreate from './UserCreate';
+import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
 
 class App extends React.Component {
   state = { language: 'english' };
@@ -16,7 +18,12 @@ class App extends React.Component {
           <i className="flag us" onClick={() => this.onLanguageChange('english')} />
           <i className="flag pl" onClick={() => this.onLanguageChange('polish')} />
         </div>
-        <UserCreate />
+        <ColorContext.Provider value="primary" >
+          <LanguageContext.Provider value={this.state.language}>
+            <UserCreate />
+          </LanguageContext.Provider>
+        </ColorContext.Provider>
+
       </div>
     )
   }
